ed3b76ffbbf7bafad8c97e568069dd14d453ae47
Raphael Malburg <raphael.malburg@gmail.com>
2024-12-11 21:59:12 +0000

    build errors fix 2

--- app/dashboard/insights/page.tsx
+++ app/dashboard/insights/page.tsx
@@ -1,12 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { checkUser } from "@/lib/checkUser";
 import Insights from "@/components/features/dashboard/metrics/Insights";
 import { redirect } from "next/navigation";
 import { getAccounts } from "@/lib/actions/accounts";
 
-export default async function InsightsPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+interface PageProps {
+  searchParams: SearchParams;
+  params: Promise<Record<string, string>>;
+}
+
+export default async function InsightsPage({ searchParams, params }: PageProps) {
+  const resolvedParams = await params;
+  const resolvedSearchParams = await searchParams;
   const user = await checkUser();
   const accounts = await getAccounts();
-  const accountId = searchParams.accountId as string;
+  const accountId = resolvedSearchParams.accountId as string;
 
   if (!user) {
     redirect("/sign-in?redirect_url=/dashboard/insights");
