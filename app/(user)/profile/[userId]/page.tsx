f213982efd412aa0f1d58bbf240c981238528a57
Raphael Malburg <raphael.malburg@gmail.com>
2024-12-11 11:39:03 +0000

    deployment fix 10

--- app/(user)/profile/[userId]/page.tsx
+++ app/(user)/profile/[userId]/page.tsx
@@ -2,19 +2,18 @@ import { Metadata } from "next";
 
 export const runtime = "edge";
 
-interface PageProps {
+type PageProps = {
   params: {
     userId: string;
   };
-}
+};
 
-export default function ProfilePage({ params }: PageProps) {
+export default async function ProfilePage({ params }: Awaited<PageProps>) {
   const { userId } = params;
 
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-2xl font-bold">Profile {userId}</h1>
-      {/* Add your profile content here */}
     </div>
   );
 }
